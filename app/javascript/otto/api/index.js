d501582353862fc8fd7708a2f76e32764d9f3900
ifilonov <filonovia@gmail.com>
2021-05-28 10:48:21 +0500

    first vue hellow_world page rendering with rails

---
+++ app/javascript/otto/api/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+const token = document.head.querySelector('meta[name="csrf-token"]')
+axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content
+
+const adapter = axios.create({
+  headers: {
+    Accept: 'application/json'
+  }
+})
+
+const api = {
+  xml: {
+    addXml: (file) => adapter.post('/females', file)
+  }
+}
+
+Vue.prototype.$api = api
---
+++ app/javascript/otto/quasar/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import Quasar from 'quasar'
+import 'quasar/dist/quasar.sass'
+import iconSet from 'quasar/icon-set/material-icons'
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+import '@quasar/extras/material-icons/material-icons.css'
+
+Vue.use(Quasar, {
+    iconSet: iconSet
+})
+
