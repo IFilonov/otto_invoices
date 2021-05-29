d8be2cddbdb09c425e1cc7596e59476b19be77c1
ifilonov <filonovia@gmail.com>
2021-05-29 23:47:36 +0500

    create method for process xml file

--- app/controllers/api/batch_files_controller.rb
+++ app/controllers/api/batch_files_controller.rb
@@ -1,9 +1,9 @@
-class Api::FilesController < ApplicationController
+class Api::BatchFilesController < ApplicationController
+  rescue_from ActiveRecord::RecordNotUnique, with: :record_not_uniq
+
   def create
-    guid = Hash.from_xml(file_params[:invoice_file].read)['Root']['FileAttribute']['GUID']
-    check_guid(guid)
-    sleep 10
-    render json: nil
+    process_batch_file(Hash.from_xml(file_params[:invoice_file].read).deep_symbolize_keys)
+    render json: @batch_file, status: :created
   end
 
   private
@@ -12,7 +12,29 @@ class Api::FilesController < ApplicationController
     params.permit(:invoice_file)
   end
 
-  def check_guid
+  def process_batch_file(file)
+    ActiveRecord::Base.transaction do
+      @batch_file = BatchFile.create(guid: file[:Root][:FileAttribute][:GUID],
+                                     batch_id: file[:Root][:FileData][:Batch][:BatchID])
+      invoices = file[:Root][:FileData][:Invoice]
+      invoices = [invoices] if invoices.instance_of?(Hash)
+      invoices.each do |invoice|
+        operation = invoice[:InvoiceOperation]
+        inv = @batch_file.invoices.create(CompanyCode: operation[:CompanyCode],
+                                          InvoiceOperationNumber: operation[:InvoiceOperationNumber],
+                                          InvoiceOperationDate: operation[:InvoiceOperationDate])
+        invoice_data = invoice[:InvoiceData]
+        invoice_data = [invoice_data] if invoice_data.instance_of?(Hash)
+        invoice_data.each do |data|
+          inv.invoice_operations.create(ParcelCode: data[:ParcelCode],
+                                        ItemQty: data[:ItemQty],
+                                        ParcelPrice: data[:ParcelPrice])
+        end
+      end
+    end
+  end
 
+  def record_not_uniq(error)
+    render json: { error: error.message }, status: :unprocessable_entity
   end
 end
