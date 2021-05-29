946f54f061cbd73245d09c0b8bf3c964eb83ee48
ifilonov <filonovia@gmail.com>
2021-05-29 14:32:33 +0500

    change sqlite to postgresql (my mistake of project creation)

---
+++ app/controllers/api/files_controller.rb
@@ -0,0 +1,18 @@
+class Api::FilesController < ApplicationController
+  def create
+    guid = Hash.from_xml(file_params[:invoice_file].read)['Root']['FileAttribute']['GUID']
+    check_guid(guid)
+    sleep 10
+    render json: nil
+  end
+
+  private
+
+  def file_params
+    params.permit(:invoice_file)
+  end
+
+  def check_guid
+    
+  end
+end
