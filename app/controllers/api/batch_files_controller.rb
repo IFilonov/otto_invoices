8e74104be48234d215ffecbd4f88344a718bf783
ifilonov <filonovia@gmail.com>
2021-05-30 13:50:14 +0500

    add two validations for invoices data

--- app/controllers/api/batch_files_controller.rb
+++ app/controllers/api/batch_files_controller.rb
@@ -15,20 +15,20 @@ class Api::BatchFilesController < ApplicationController
   def process_batch_file(file)
     ActiveRecord::Base.transaction do
       @batch_file = BatchFile.create!(guid: file[:Root][:FileAttribute][:GUID],
-                                     batch_id: file[:Root][:FileData][:Batch][:BatchID])
+                                      batch_id: file[:Root][:FileData][:Batch][:BatchID])
       invoices = file[:Root][:FileData][:Invoice]
       invoices = [invoices] if invoices.instance_of?(Hash)
       invoices.each do |invoice|
         operation = invoice[:InvoiceOperation]
         inv = @batch_file.invoices.create!(CompanyCode: operation[:CompanyCode],
-                                          InvoiceOperationNumber: operation[:InvoiceOperationNumber],
-                                          InvoiceOperationDate: operation[:InvoiceOperationDate])
+                                           InvoiceOperationNumber: operation[:InvoiceOperationNumber],
+                                           InvoiceOperationDate: operation[:InvoiceOperationDate])
         invoice_data = invoice[:InvoiceData]
         invoice_data = [invoice_data] if invoice_data.instance_of?(Hash)
         invoice_data.each do |data|
           inv.invoice_operations.create!(ParcelCode: data[:ParcelCode],
-                                        ItemQty: data[:ItemQty],
-                                        ParcelPrice: data[:ParcelPrice])
+                                         ItemQty: data[:ItemQty],
+                                         ParcelPrice: data[:ParcelPrice])
         end
       end
     end
--- app/models/invoice_operation.rb
+++ app/models/invoice_operation.rb
@@ -1,7 +1,14 @@
 class InvoiceOperation < ApplicationRecord
   belongs_to :invoice
   validates :ParcelCode, :ItemQty, :ParcelPrice, presence: true
-  validates :ParcelCode, length: { is: 15 }
+  validates :ParcelCode,
+            format: { with: /\A\d{15}\z/, message: 'Only 15 digits are possible in ParcelCode' }
   validates :ItemQty, numericality: { less_than_or_equal_to: 50, only_integer: true }
-  validates :ParcelPrice, numericality: { less_than_or_equal_to: 90000 }
+  validates :ParcelPrice, numericality: { less_than_or_equal_to: 90_000 }
+  validate :max_parcels, on: :create
+
+  def max_parcels
+    err_msg = 'Cannot add more then 10 parcels'
+    errors.add(:invoice_id, :too_many_parcels, message: err_msg) if invoice && invoice.invoice_operations.count >= 10
+  end
 end
